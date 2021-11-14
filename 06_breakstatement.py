05f31674379b46c260890c807a9a110720d2584c
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-11-14 16:53:09 +0530

    Add files via upload

---
+++ 06_breakstatement.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    print(i)
+    if i==5:
+     break
+else:
+    print("This is inside else of for")
+
+    # In This Case Else Part Is Not Executed bcs
+    #  it is not the sucessfull Termination of forLoop
